33820ed891446351447f107d9f192fecb6131be3
Frederick Clay <fredfmclay@gmail.com>
2022-09-18 19:38:50 +0100

    project structured

---
+++ front-end/src/components/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+
+    return (
+        <div>
+            <h1>Grettings, Traveller!</h1>
+            <p>This is a MERN stack app that allows you to learn more about the history of Formula 1.</p>
+        </div>
+    )
+
+};
+
+export default HomePage;
---
+++ front-end/src/components/structure/Header.jsx
@@ -0,0 +1,13 @@
+import style from '../css/Header.module.css';
+
+const Header = () => {
+
+    return (
+        <div className={style.AppHeader}>
+            <h1>EVERYTHING F1</h1>
+        </div>
+    )
+
+};
+
+export default Header;
