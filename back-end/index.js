30f625a79d8d41c2876d7a683abce124129c8049
Frederick Clay <fredfmclay@gmail.com>
2022-10-13 14:29:59 +0100

    added login

--- back-end/index.js
+++ back-end/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+mongoose.connect(`${process.env.MONGOPREFIX}${process.env.USER}:${process.env.PW}${process.env.MONGOSUFFIX}`, { useNewUrlParser: true}, (err) => {
+    if (err) return console.log(err);
+    return console.log('Connection Successful!');
+});
 
 const app = express();
 
@@ -10,6 +19,9 @@ app.use(bodyParser.json());
 
 const teamRoutes = require('./controllers/teamsController');
 app.use('/teams', teamRoutes);
+
+const userRoutes = require('./controllers/userController');
+app.use('/users', userRoutes);
 
 app.get('/greeting', (req, res) => {
     res.send("Welcome to Fred's Formula 1 Driver API!")
